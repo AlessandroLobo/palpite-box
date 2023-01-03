e06c48bbe6dc8c7f4829c0ab4ba05df2add2005f
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-01-03 07:57:40 -0300

    resolvendo problemas do deploy5

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,42 +1,36 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { fromBase64 } from '../../utils/base64'
+
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 
-
-const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID);
-
-export const fromBase64 = value => {
-    const buff = Buffer.from(value, 'base64');
-    return buff.toString('ascii');
-}
 
 export default async (req, res) => {
-    //console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            //private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
-            private_key: process.env.SHEET_PRIVATE_KEY
-        });
-        await doc.loadInfo();
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
+        })
+        await doc.loadInfo()
 
-        const sheet = doc.sheetsByIndex[2];
-        await sheet.loadCells("A2:B2");
+        const sheet = doc.sheetsByIndex[2]
+        await sheet.loadCells('A2:B2')
 
-        const mostrarPromocaoCell = sheet.getCell(1, 0);
-        const textoCell = sheet.getCell(1, 1);
+        const mostrarPromocaoCell = sheet.getCell(1, 0)
+        const textoCell = sheet.getCell(1, 1)
 
-        res.end(
-            JSON.stringify({
-                showCoupon: mostrarPromocaoCell.value === "VERDADEIRO",
-                message: textoCell.value,
-            })
-        );
+        res.end(JSON.stringify({
+            showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
+            message: textoCell.value
+        }))
+
     } catch (err) {
-        res.end(
-            JSON.stringify({
-                showCoupon: false,
-                message: "",
-            })
-        );
+        res.end(JSON.stringify({
+            showCoupon: false,
+            message: ''
+        }))
     }
-};
+
+
+
+}
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,59 +1,54 @@
-import { GoogleSpreadsheet } from "google-spreadsheet";
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import moment from 'moment'
+import { fromBase64 } from '../../utils/base64'
 
-
-import moment from "moment/moment";
-
-const doc = new GoogleSpreadsheet(
-  "1clQ6pBZhoxR-yDYgaU2AFMMiDxoKwBsbaSC7voUE5pU"
-);
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 const genCupom = () => {
-  const code = parseInt(moment().format("YYMMDDhmmssSSS"))
-    .toString(16)
-    .toLocaleUpperCase();
-  return code.substr(0, 4) + "-" + code.substr(4, 4) + "-" + code.substr(8, 4);
-};
+  const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
+  return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
+}
 
 export default async (req, res) => {
   try {
-    await doc.useServiceAccountAuth(credentials);
-    await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[1];
-    const data = JSON.parse(req.body);
+    await doc.useServiceAccountAuth({
+      client_email: process.env.SHEET_CLIENT_EMAIL,
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
+    })
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1]
+    const data = JSON.parse(req.body)
 
-    const sheetConfig = doc.sheetsByIndex[2];
-    await sheetConfig.loadCells("A2:B2");
+    const sheetConfig = doc.sheetsByIndex[2]
+    await sheetConfig.loadCells('A2:B2')
 
-    const mostrarPromocaoCell = sheetConfig.getCell(1, 0);
-    const textoCell = sheetConfig.getCell(1, 1);
+    const mostrarPromocaoCell = sheetConfig.getCell(1, 0)
+    const textoCell = sheetConfig.getCell(1, 1)
 
-    let Cupom = "";
-    let Promo = "";
-
-    if (mostrarPromocaoCell.value === "VERDADEIRO") {
-      Cupom = genCupom();
-      Promo = textoCell.value;
+    let Cupom = ''
+    let Promo = ''
+    if (mostrarPromocaoCell.value === 'VERDADEIRO') {
+      Cupom = genCupom()
+      Promo = textoCell.value
     }
 
-    //Nome	Email	Whatsapp	Cupom	Promo
+    // Nome	Email	Whatsapp	Cupom	Promo
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
       Nota: parseInt(data.Nota),
-      "Data Preenchimento": new moment().format("DD/MM/YYYY, h:mm:ss a"),
+      'Data Preenchimento': moment().format('DD/MM/YYYY HH:mm:ss'),
       Cupom,
-      Promo,
-    });
-    res.end(
-      JSON.stringify({
-        showCupon: Cupom !== "",
-        Cupom,
-        Promo,
-      })
-    );
+      Promo
+    })
+    res.end(JSON.stringify({
+      showCoupon: Cupom !== '',
+      Cupom,
+      Promo
+    }))
   } catch (err) {
-    console.log(err);
-    res.end("error");
+    console.log(err)
+    res.end('error')
   }
-};
+}
